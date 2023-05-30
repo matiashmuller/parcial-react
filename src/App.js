71e19e8ed364fc176a2abd28ff4fc6e0dc6e066e
Matías <mullermatiash@gmail.com>
2023-05-29 21:23:30 -0300

    inicio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
 import './App.css';
-//importo componente para que esté disponible para utilizarlo
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
-//importar hook
-import { useState } from 'react';
 
 function App() {
-
-  //Creamos hook (este es de estado) [nombre de cte, función modificadora]
-  const [servicios, agregarServicio] = useState(
-    //Crear lista (de servicios en este caso), entre llaves sus valores
-    [
-      { id: 1, nombre: "Goya", precio: 2500 },
-      { id: 2, nombre: "Dalí", precio: 2000 },
-      { id: 3, nombre: "Monet", precio: 3000 },
-    ]
-  );
-
 
   return (
     <div>
